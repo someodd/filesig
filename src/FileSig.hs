44b36338a513785b290bd963010ad53898d94e28
Felix Paulusma <felix.paulusma@gmail.com>
2020-05-14 00:30:40 +0200

    large overhaul of library code for readability and getting rid of redundancy

--- src/FileSig.hs
+++ src/FileSig.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 -- | Discern a file's type using hex-encoded file signatures.
 --
@@ -12,29 +12,22 @@ module FileSig
   , prettyMatchPrint
   )  where
 
-import Data.Foldable
-import Data.FileEmbed
+-- import Control.Monad.IO.Class
+-- import Data.Aeson
+-- import Data.Aeson.Types
+-- import Data.FileEmbed
+-- import qualified Data.ByteString.Lazy as B
+-- import Data.Vector as V
 import Paths_filesig
-import Data.Vector as V
-import Data.Maybe
-import Data.Functor
-import Data.Traversable
-import Control.Monad
-import Data.Text as T
-import Data.HashMap.Strict as HM
-import Data.Aeson
-import Data.Aeson.Types
-import Control.Monad.IO.Class
-import Data.List                            as L
-import Data.Ord
-import qualified Data.Hex                   as H
 import qualified Data.ByteString            as BS
-import qualified Data.ByteString.Char8      as B8
-import qualified Data.ByteString.Lazy as B
+import Data.ByteString.Base16 (decodeBase16Lenient)
+import Data.HashMap.Strict as HM
+import Data.Ord (Down(..))
+import Data.Text as T
 
--- | Magic bytes/file signature, in the form of the hex (`String` [no spaces]), followed
--- by the offset (`Int`).
-type Signature = (String, Int)
+-- | Magic bytes/file signature, in the form of the offset (`Int`), followed
+-- by the bytes (`ByteString`).
+type Signature = (Int, ByteString)
 
 -- | The extension of a file format, which acts as a key in the magic database.
 type ExtensionKey = Text
@@ -50,7 +43,7 @@ data FileFormat = FileFormat {
 -- | The abstraction of the JSON magic database containing all the file signatures.
 -- The key is the file extension.
 type MagicMap = HM.HashMap ExtensionKey FileFormat
-
+{-
 -- | JSON parser for a file format entries in the magic database.
 instance FromJSON FileFormat where
   parseJSON = withObject "FileFormat" $ \o ->
@@ -80,53 +73,72 @@ decodedJSON :: IO MagicMap
 decodedJSON = do
   let (Right daJSON) = eitherDecode' getJSON :: Either String MagicMap
   pure daJSON
+-}
 
--- | Slice a ByteString.
-byteSlice :: Int -> Int -> BS.ByteString -> BS.ByteString
-byteSlice from to xs = BS.take (to - from + 1) (BS.drop from xs)
+-- | This is the entire MagicMap to be used everywhere, no IO required.
+--
+-- The 'decodeBase16Lenient' interprets the hexidecimal into raw bytestring,
+-- So it is already in optimal form at runtime.
+magicMap :: MagicMap
+magicMap =
+    HM.map go rawMagicMap
+  where
+    go (sigs, mime) = (decodeBase16Lenient <$> sigs, mime)
+
+allExtensions :: [ExtensionKey]
+allExtensions = HM.keys magicMap
+
+longestSignature :: ByteString
+longestSignature = maximumBy (compare `on` BS.length) allSigs
+  where
+    allSigs = concatMap (fmap snd . fst) $ HM.elems magicMap
 
 -- | Is the contents the file type defined (by the supplied extension)? Nothing
 -- if the file extension supplied doesn't exist in the `HashMap`.
-hasSignature' :: MagicMap -> BS.ByteString -> Text -> Maybe Bool
-hasSignature' magicMap contents fileExtension =
-  let extEntry = HM.lookup fileExtension magicMap
-  in  case extEntry of
-    (Just signatures) -> Just $ L.or $ L.map checkOneSig (magicBytes signatures) -- FIXME: Just fileExt
-    Nothing           -> Nothing 
+hasSignature' :: Text -> BS.ByteString -> Bool
+hasSignature' fileExtension contents =
+    -- If the ext doesn't exist, we can't verify it,
+    -- so we default to 'False'.
+    maybe False (anyValidSig . magicBytes) mFileExt
   where
-    checkOneSig sig =
-      let (expectedSig, offset) = sig
-          expectedSigBS = B8.pack expectedSig
-          potentialSig = byteSlice offset (offset + (BS.length expectedSigBS `div` 2) - 1) contents
-      in  H.hex potentialSig == expectedSigBS
+    anyValidSig = or . fmap checkOneSig
+    mFileExt = HM.lookup fileExtension magicMap
+    checkOneSig (offset, expectedSig) =
+      let potentialSig = BS.take (BS.length expectedSig) $ BS.drop offset contents
+      in  potentialSig == expectedSig
 
 -- | Check if the file at the suppiled `FilePath` matches a file format from the database.
 -- `Nothing` if no such format (specified by extension) exists in the database.
-hasSignature :: MagicMap -> FilePath -> Text -> IO (Maybe Bool)
-hasSignature magicMap filePath fileExtension = do
-  daFile <- BS.readFile filePath
-  pure $ hasSignature' magicMap daFile fileExtension
+hasSignature :: Text -> FilePath -> IO Bool
+hasSignature fileExtension =
+    fmap (hasSignature' fileExtension) . efficientRead
+  where
+    -- TODO: This should use something that
+    -- doesn't read the entire file into memory
+    efficientRead = BS.readFile
 
+{-
 getAllExtensions :: MagicMap -> [Text]
 getAllExtensions magicMap = HM.keys magicMap
+-}
 
 -- FIXME: collect all that match and then reutrn the longest one?
 -- | Check if the supplied `FilePath` matches any of the file formats in the magic database.
-signatureMatch :: HM.HashMap Text FileFormat -> FilePath -> IO [ExtensionKey]
-signatureMatch magicMap filePath = do
+signatureMatch :: FilePath -> IO [ExtensionKey]
+signatureMatch filePath = do
   contents <- BS.readFile filePath
   -- FIXME/TODO: sort by the longest signature
-  pure $ L.filter (\x -> fromMaybe False (hasSignature' magicMap contents x)) (getAllExtensions magicMap)
+  pure $ filter (flip hasSignature' contents) allExtensions
 
-prettyMatchPrint :: MagicMap -> [ExtensionKey] -> IO ()
-prettyMatchPrint magicMap exts = traverse_ prettyOneMatch exts
+prettyMatchPrint :: [ExtensionKey] -> IO ()
+prettyMatchPrint = traverse_ prettyOneMatch
   where
-  prettyOneMatch ext =
-    let entry = HM.lookup ext magicMap
-    in  case entry of
-      (Just fileFormat) ->
-        let typicalExt = "Typical Extension: ." L.++ (T.unpack ext)
-            mediaType  = "MIME/Media Type:   " L.++ (T.unpack $ mime fileFormat)
-            signatures = "Signatures:        " L.++ (show $ magicBytes fileFormat)
-        in  putStrLn $ typicalExt L.++ "\n" L.++ mediaType L.++ "\n" L.++ signatures L.++ "\n"
-      Nothing  -> error "This shouldn't be possible!"
+    prettyOneMatch ext =
+      case HM.lookup ext magicMap of
+        Just (FileFormat sigs mime) ->
+          let typicalExt = "Typical Extension: ." <> ext
+              mediaType  = "MIME/Media Type:   " <> mime
+              signatures = "Signatures:        " <> T.pack (show sigs)
+              putTextLn = putStrLn . T.unpack
+          in  putTextLn $ T.unlines [typicalExt, mediaType, signatures] <> "\n"
+        Nothing -> putTextLn $ "No support for this extension: ." <> ext
