2cb0abca3ad5331a1270e43b33513b7663696098
Felix Paulusma <felix.paulusma@gmail.com>
2020-05-14 00:31:06 +0200

    other cleanups and getting rid of now unnecessary code

--- test/MyLibTest.hs
+++ test/MyLibTest.hs
@@ -3,42 +3,38 @@
 -- will at least output to a log that gives accurate results.
 module Main (main) where
 
-import Data.Maybe
-import qualified Data.Text as T
-
 import Test.Tasty
 import Test.Tasty.HUnit
 
 import Paths_filesig
 import FileSig
 
-testHasSignature :: TestTree
-testHasSignature = testCase "hasSignature" $ do
-  magic <- decodedJSON
 
-  scriptPath <- getDataFileName "test/fixtures/rtf.rtf"
-  scriptBool <- hasSignature (magic) scriptPath "rtf"
-  assertBool "Failed RTF" (isJust scriptBool && fromJust scriptBool)
-
-  docPath <- getDataFileName "test/fixtures/doc.doc"
-  docBool <- hasSignature (magic) docPath "doc"
-  assertBool "Failed doc" (isJust docBool && fromJust docBool)
-
-
-testSignatureMatch :: TestTree
-testSignatureMatch = testCase "signatureMatch" $ do
-  magic <- decodedJSON
-
-  rtfPath <- getDataFileName "test/fixtures/rtf.rtf"
-  match <- signatureMatch magic rtfPath
-  assertEqual "Failed signatureMatch (rtf)" ["rtf"] match
-
-testCount :: TestTree
-testCount = testCase "Count JSON entries" $ do
-  magic <- decodedJSON
-  assertEqual "Count entries" 150 (length $ getAllExtensions magic)
+main :: IO ()
+main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests" [testHasSignature, testSignatureMatch, testCount]
 
-main = defaultMain tests
+testHasSignature :: TestTree
+testHasSignature = testCase "hasSignature" $ do
+
+  scriptPath <- getDataFileName "test/fixtures/rtf.rtf"
+  scriptBool <- hasSignature scriptPath "rtf"
+  assertBool "Failed RTF" scriptBool
+
+  docPath <- getDataFileName "test/fixtures/doc.doc"
+  docBool <- hasSignature docPath "doc"
+  assertBool "Failed doc" docBool
+
+
+testSignatureMatch :: TestTree
+testSignatureMatch = testCase "signatureMatch" $ do
+
+  rtfPath <- getDataFileName "test/fixtures/rtf.rtf"
+  match <- signatureMatch rtfPath
+  assertEqual "Failed signatureMatch (rtf)" ["rtf"] match
+
+testCount :: TestTree
+testCount = testCase "Count JSON entries" $
+  assertEqual "Count entries" 150 (length allExtensions)
